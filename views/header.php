488f8295069afbfa64db1035190fc9aa6d3a266b
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-06-27 19:34:06 +0200

    refresh meta deleted

--- views/header.php
+++ views/header.php
@@ -6,7 +6,6 @@ session_start();
 <html>
 <head>
    <meta charset="utf-8">
-   <meta http-equiv="refresh" content="<?php echo Session::get('refresh-time')?>;URL='<?php echo $_SERVER['PHP_SELF']?>'">
    <title><?= $data['title'] . ' - ' . SITETITLE ?></title>
    <link rel="stylesheet" href="<?= URL::STYLES('bootstrap.min') ?>">
    <link rel="stylesheet" href="<?= URL::STYLES('style') ?>">
